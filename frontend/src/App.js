b32be93c3806015c7df858b8537c7e1ea13bff7c
Rajeshwar <kolimirajeshwarreddy@gmail.com>
2024-02-03 23:18:16 +0530

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,14 @@ import Chat from "./Components/Chat";
 function App() {
     return (
         <div className="App">
-            Hello
-            {/* <BrowserRouter>
+            {/* Hello */}
+            <BrowserRouter>
                 <Routes>
                     <Route index element={<Dashboard />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/:username/:friend" element={<Chat />} />
                 </Routes>
-            </BrowserRouter> */}
+            </BrowserRouter>
         </div>
     );
 }
