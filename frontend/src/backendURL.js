01a8fc2ec2557a097f8f1f7c67a9198d208480b4
Rajeshwar <kolimirajeshwarreddy@gmail.com>
2024-02-24 02:04:04 +0530

    Major updates

--- frontend/src/backendURL.js
+++ frontend/src/backendURL.js
@@ -1,3 +1,3 @@
-const url = `http://localhost:4000`;
-// const url = `https://chatapplication-rza4.onrender.com`;
+// const url = `http://localhost:4000`;
+const url = `https://chatapplication-rza4.onrender.com`;
 export default url;
